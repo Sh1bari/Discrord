ceea90339ef9e297230449fc3003bc4fb07e8fd7
Sh1bari <vova_krasnov_2004@mail.ru>
2024-10-09 02:38:29 +0300

    init

--- src/main/java/com/example/backend/VoiceWebSocketHandler.java
+++ src/main/java/com/example/backend/VoiceWebSocketHandler.java
@@ -1,38 +1,61 @@
 package com.example.backend;
 
 import lombok.*;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.web.socket.BinaryMessage;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.BinaryWebSocketHandler;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import java.io.IOException;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
+@Component
+public class VoiceWebSocketHandler extends BinaryWebSocketHandler {
 
-public class VoiceWebSocketHandler extends TextWebSocketHandler {
-
-    private static final CopyOnWriteArrayList<WebSocketSession> sessions = new CopyOnWriteArrayList<>();
+    // Карта комнат и их сессий
+    private final Map<String, Set<WebSocketSession>> roomSessions = new ConcurrentHashMap<>();
 
     @Override
-    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-        sessions.add(session);
+    public void afterConnectionEstablished(WebSocketSession session) {
+        String room = getRoom(session);
+        roomSessions.computeIfAbsent(room, k -> ConcurrentHashMap.newKeySet()).add(session);
     }
 
     @Override
-    public void handleBinaryMessage(WebSocketSession session, BinaryMessage message) {
-        for (WebSocketSession wsSession : sessions) {
-            if (wsSession.isOpen() && !session.getId().equals(wsSession.getId())) {
-                try {
-                    wsSession.sendMessage(message);
-                } catch (IOException e) {
-                    e.printStackTrace();
+    protected void handleBinaryMessage(WebSocketSession session, BinaryMessage message) {
+        String room = getRoom(session);
+        Set<WebSocketSession> sessions = roomSessions.get(room);
+        if (sessions != null) {
+            for (WebSocketSession wsSession : sessions) {
+                if (wsSession.isOpen() && !session.getId().equals(wsSession.getId())) {
+                    try {
+                        wsSession.sendMessage(message);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
                 }
             }
         }
     }
 
     @Override
-    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-        sessions.remove(session);
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) {
+        String room = getRoom(session);
+        Set<WebSocketSession> sessions = roomSessions.get(room);
+        if (sessions != null) {
+            sessions.remove(session);
+            if (sessions.isEmpty()) {
+                roomSessions.remove(room);
+            }
+        }
+    }
+
+    private String getRoom(WebSocketSession session) {
+        return (String) session.getAttributes().get("room");
     }
 }
--- src/main/java/com/example/backend/configs/WebSocketConfig.java
+++ src/main/java/com/example/backend/configs/WebSocketConfig.java
@@ -1,17 +1,24 @@
 package com.example.backend.configs;
 
+import com.example.backend.RoomHandshakeInterceptor;
 import com.example.backend.VoiceWebSocketHandler;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 @Configuration
+@RequiredArgsConstructor
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
+    private final VoiceWebSocketHandler voiceWebSocketHandler;
+    private final RoomHandshakeInterceptor roomHandshakeInterceptor;
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(new VoiceWebSocketHandler(), "/voice").setAllowedOrigins("*");
+        registry.addHandler(voiceWebSocketHandler, "/voice/{room}")
+                .setAllowedOrigins("*")
+                .addInterceptors(roomHandshakeInterceptor);
     }
 }
